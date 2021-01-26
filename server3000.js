c1795c49d5eff392963b2c2c2d4bd37318410fb9
pornoiya <lactnovpidor@gmail.com>
2021-01-26 13:30:58 +0500

    Initial commit

---
+++ server3000.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+const server = new http.Server();
+
+server.listen(3000, '127.0.0.1');
+server.on('request', function (req, res) {
+    res.setHeader('content-type', 'text/html');
+    fs.readFile("index.html", { encoding: 'utf-8' }, function (err, data) {
+        if (err) {
+            console.error(err.message);
+        } else {
+            res.end(data);
+        }
+    });
+});
---
+++ server4200.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const server = new http.Server();
+server.listen(4200, '127.0.0.1');
+server.on('request', function(req, res) {
+
+    const urlParsed = url.parse(req.url, true);
+
+    if (urlParsed.pathname === '/img') {
+        fs.readFile("img.jpg", function(err, data){
+            if (err) {
+                console.error(err.message);
+            } else {
+                let call = urlParsed.query.callback;
+                eval(call);
+                onUserData(JSON.stringify({"Id": 10, "Name": "Thorn", "RegistrationDate": "2020-01-12" }));
+                res.end(data);
+            }
+        });
+    }
+
+    if (urlParsed.pathname === '/otherimg') {
+        fs.readFile("img.jpg", function (err, data){
+            if(err){
+                console.error(err.message);
+            }else{
+                res.end(data);
+            }
+        });
+    }
+
+    if (urlParsed.pathname === '/content'){
+        fs.readFile("content.txt", {encoding: 'utf-8'}, function(err, data){
+            if(err){
+                console.error(err.message);
+            }else{
+                res.setHeader("Access-Control-Allow-Origin",
+                    "http://localhost:3000");
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Headers","Origin, " +
+                    "Content-Type, X-Auth-Token, Authorization");
+                res.end(data);
+            }
+        });
+    }
+    else {
+        let call = urlParsed.query.callback;
+        eval(call);
+        onUserData(JSON.stringify({"Id": 10, "Name": "Thorn", "RegistrationDate": "2020-01-12" }));
+    }
+});
